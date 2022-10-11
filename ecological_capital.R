309987aa8dcc64ca05825c4baf40c29067620724
Ian Eddy <ianmseddy@gmail.com>
2022-10-11 15:46:59 -0700

    cleanup

--- ecological_capital.R
+++ ecological_capital.R
@@ -1,25 +1,25 @@
 #ecological capital 
 
-provSpp <- sf::st_read(file.path(currentData, "RoF/Ontario Geohub/PROVTRKG/Non_Sensitive.gdb"), 
-                       layer = "PROV_TRK_SPECIES_1KM_GRID")
-provDT <- sf::st_read(file.path(currentData, "RoF/Ontario Geohub/PROVTRKG/Non_Sensitive.gdb"), 
-                      layer = "PROV_TRK_SPECIES_GRID_DETAIL")
-provSpp <- merge(provSpp, provDT, by.x = "OGF_ID", by.y = "PROV_TRK_SPECIES_1KM_GRID_ID")
-provSpp1 <- provSpp[!is.na(provSpp$COSEWIC_STATUS),]
-rof <- st_transform(rof, crs = st_crs(provSpp1))
-provSpp1 <- provSpp1[rof,]
-# plot(provSpp1["COSEWIC_STATUS"], key.pos = 2, lwd = 0)
-ggplot(provSpp1) + geom_sf(aes(fill = COSEWIC_STATUS), colour = NA) + theme_bw() + 
-  geom_sf(data = rof, color = "grey", fill = NA)
+# provSpp <- sf::st_read(file.path(currentData, "RoF/Ontario Geohub/PROVTRKG/Non_Sensitive.gdb"), 
+#                        layer = "PROV_TRK_SPECIES_1KM_GRID")
+# provDT <- sf::st_read(file.path(currentData, "RoF/Ontario Geohub/PROVTRKG/Non_Sensitive.gdb"), 
+#                       layer = "PROV_TRK_SPECIES_GRID_DETAIL")
+# provSpp <- merge(provSpp, provDT, by.x = "OGF_ID", by.y = "PROV_TRK_SPECIES_1KM_GRID_ID")
+# provSpp1 <- provSpp[!is.na(provSpp$COSEWIC_STATUS),]
+# rof <- st_transform(rof, crs = st_crs(provSpp1))
+# provSpp1 <- provSpp1[rof,]
+# # plot(provSpp1["COSEWIC_STATUS"], key.pos = 2, lwd = 0)
+# ggplot(provSpp1) + geom_sf(aes(fill = COSEWIC_STATUS), colour = NA) + theme_bw() + 
+#   geom_sf(data = rof, color = "grey", fill = NA)
+# 
+# #some basic statistics
+# tempdf <- as.data.table(provSpp1)
+# tempdf[COSEWIC_STATUS != "NAR", .N, .(COMMON_NAME)]
+# table(provSpp1$COSEWIC_STATUS)
+# provSpp1[provSpp1$COSEWIC_STATUS == "END",]$COMMON_NAME
 
-#some basic statistics
-tempdf <- as.data.table(provSpp1)
-tempdf[COSEWIC_STATUS != "NAR", .N, .(COMMON_NAME)]
-table(provSpp1$COSEWIC_STATUS)
-provSpp1[provSpp1$COSEWIC_STATUS == "END",]$COMMON_NAME
-
-natureCounts <- fread(file.path(currentData, paste0("Wildlife/Ontario/naturecounts_full_obba2be_summ_",
-                                                    "1652219045296/naturecounts_data.txt")))
+# natureCounts <- fread(file.path(currentData, paste0("Wildlife/Ontario/naturecounts_full_obba2be_summ_",
+                                                    # "1652219045296/naturecounts_data.txt")))
 ####Biomass from kNN 
 
 OntarioSA <- st_as_sf(Ontario)
@@ -58,12 +58,38 @@ histelC <- prepInputs(url = "https://bolin.su.se/data/uploads/hugelius-2021-1.zi
                       targetFile = "Histel_SOC_hg_per_sqm_WGS84.tif",
                       rasterToMatch = RTMraster,
                       filename2 = "histel_SOC_hg_sqm_Ontario.tif")
-# unharvested forest
+#agriculture
+classLegend <- data.table(className = c("Other", "cloud/shadow", "clear open water", "turbid water", 
+                                        "shoreline", "mudflats", "marsh", "swamp", "fen", "bog", 
+                                        "heath", "sparse treed", "treed upland", "deciduous treed",
+                                        "mixed treed", "coniferous treed", "plantations - treed cultivated", 
+                                        "hedge rows", "disturbance", "cliff and talus", "alvar", 
+                                        "sand barren and dune", "open tallgrass prairie", 
+                                        "tallgrass savannah", "tallgrass woodland", 
+                                        "sand/gravel/mine tailings/extraction", 
+                                        "bedrock", "community/infrastructure", 
+                                        "agriculture and undifferentiated rural land use"), 
+                          class = c(-99, -9, 1:8, 10:28))
 
 
 
+lcc <- "data/OLCC_V2_Ontario.tif"
+if (file.exists(lcc)){
+  lcc <- rast(lcc) 
+} else {
+  lccraw <- rast("data/OLCC_V2.tif")
+  lcc <- terra::project(lccraw, y = RTMrast,
+                        method = 'near', mask = TRUE, 
+                        filename = lcc, overwrite = TRUE)
+}
 
-#wildlife values sites
-# 
-
-#water pur
+if (file.exists("outputs/agriculturalLC.tif")) {
+  ag <- rast("outputs/agriculturalLC.tif")
+} else {
+  
+  lccVals <- as.data.table(lcc[])
+  lccVals[, newVal := ifelse(OLCC_V2 == 28, 1, 0)]
+  lccVals[is.na(OLCC_V2), newVal := NA]
+  ag <- terra::init(lcc, lccVals$newVal)
+  terra::write.raster(ag, "outputs/agriculturalLC.tif")
+}             
--- risk_source_intensity.R
+++ risk_source_intensity.R
@@ -3,25 +3,22 @@
 #I am trying to create a raster with non-empty cells representing lines
 
 #generate a road raster
-if (!file.exists("outputs/distance_to_roads.tif")) {
-  roads <- vect(file.path(currentData, "Infrastructure/roads/Ontario_Road_Network_(ORN)_Road_NET_Element",
-                          "Ontario_Road_Network_(ORN)_Road_Net_Element.shp"))
-  roads <- project(roads, RTMrast)
-  roads <- crop(roads, RTMrast)
-  roads <- buffer(x = roads, width = 250)
-  roadTemplate <- raster::raster(RTMraster)
-  roads <- st_as_sf(roads)  %>%
-    st_cast(., "POLYGON")
-  roads$road <- 1
-  roadRaster <- fasterize::fasterize(sf = roads, raster = roadTemplate, 
-                                     field = "road", background = NA)
-  roadRaster <- terra::rast(roadRaster)
-  roadRaster <- reproducible::Cache(gridDistance, temp, userTags = c("gridDistance")) 
-  roadDistance <- mask(roadRaster, Ontario, filename = "outputs/distance_to_roads.tif")
-  rm(test, temp)
-} else {
-  roadDistance <- rast("outputs/distance_to_roads.tif")
-}
+roads <- vect(file.path(currentData, "Infrastructure/roads/Ontario_Road_Network_(ORN)_Road_NET_Element",
+                        "Ontario_Road_Network_(ORN)_Road_Net_Element.shp"))
+roads <- terra::project(roads, RTMrast)
+roads <- terra::crop(roads, RTMrast)
+roads <- terra::buffer(x = roads, width = 50)
+roadTemplate <- raster(raster::disaggregate(RTMraster, fact = c(5,5)))
+roads <- st_as_sf(roads)  %>%
+  st_cast(., "POLYGON")
+roads$road <- 1
+roadRaster <- fasterize::fasterize(sf = roads, raster = roadTemplate, 
+                                   field = "road", background = NA)
+writeRaster(roadRaster, "data/rasterized_roads_Ontario.tif", overwrite = TRUE)
+roadRaster <- terra::rast("data/rasterized_roads_Ontario.tif")
+roadMat <- terra::focalMat(x = roadRaster, d = 1000, type = "circle")
+roadDensity <- terra::focal(x = roadRaster, w = roadMat, fun = "sum", na.rm = TRUE)
+roadDensity <- terra::aggregate(roadDensity, fact = 5, fun = "mean", filename = "outputs/roadDensity_50m.tif")
 # writeRaster(roadDistance, filename = "outputs/distance_To_MNRF_roads.tif")
 #this raster is in a gdb, and I do not know a way to read a raster from gdb (sf expects vector)
 #I can extract it but not through prepInputs. Fix later
@@ -36,7 +33,7 @@ classLegend <- data.table(className = c("Other", "cloud/shadow", "clear open wat
                                         "bedrock", "community/infrastructure", 
                                         "agriculture and undifferentiated rural land use"), 
                           class = c(-99, -9, 1:8, 10:28))
-classLegend
+
 lcc <- "data/OLCC_V2_Ontario.tif"
 if (file.exists(lcc)){
   lcc <- rast(lcc) 
@@ -46,6 +43,10 @@ if (file.exists(lcc)){
                         method = 'near', mask = TRUE, 
                         filename = lcc, overwrite = TRUE)
 }
+
+lccraw
+urbanCover[urbanCover != classLegend[className == "community/infrastructure"]$class] <- NA
+
 
 #harvest - 
 if (!file.exists("data/C2C_harvest_mask_Ontario.tif")) {
@@ -59,7 +60,7 @@ if (!file.exists("data/C2C_harvest_mask_Ontario.tif")) {
 
 #maybe we do a 1 km focal radius? 
 harvestMat <- terra::focalMat(harvest, d = 1000, type = "circle", fillNA = TRUE)
-harvestIntensity <- focal(harvest, harvestMat, filename = "data/harvestIntensity_1985_2015.tif")
+harvestIntensity <- focal(harvest, harvestMat, filename = "outputs/harvestIntensity_1985_2015.tif")
 rm(harvestMat, harvest)
 # fishing and hunting areas
 
