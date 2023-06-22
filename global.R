a3022290fb8dfa01c6efc50d0599bf6efeaef6ee
Ian Eddy <ianmseddy@gmail.com>
2023-06-22 16:06:23 -0700

    get rid of natural breaks

--- global.R
+++ global.R
@@ -1,4 +1,4 @@
-# The overarching framework is derived from the Wang (2020) manuscript
+# The overarching framework is derived from the Wang (2021) manuscript
 # ‘Regional ecological risk assessment of multi‑ecosystems under the disturbance of regional pole‑axis syste:
 # a case study of the Tongjiang–Fuyuan region, China.’ 
 # It is essentially an overlay analysis using 3) categories of data:
@@ -22,4 +22,11 @@ source("risk_source.R")
 # the sources of risk include areas with higher density of roads, timber harvest, farmland, 
 # and urban development, due to presumed increaes in pollutants, fire risk, wildlife conflict, etc
 
+# following Wang et al. 2021, classify the outputs using Jenk's Natural Breaks. 
+# this can be done using rgeoda or in ArcMap with the classify function. 
+# The two approaches do not achieve the same result - evidently there is some subsampling involved
+# I suspect rgeoda is internally sampling as it is much faster. 
+# I tried alternatives like BAMMtools::getJenksBreaks but it is extremely slow
 
+filesToZip <- list.files("outputs", recursive = TRUE, full.names = TRUE, pattern = ".tif$")
+utils::zip(zipfile = "allOutputs.zip", files = filesToZip)
